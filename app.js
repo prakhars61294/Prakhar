8c3aab42c69e0b5db68692eb8c9f8d2e276fd81f
prakhars61294 <61067399+prakhars61294@users.noreply.github.com>
2020-02-25 07:40:53 +0530

    Add files via upload
    all work complete

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ const user = require('./controllers/user/user');
 const login = require('./controllers/login/login');
 const product = require('./controllers/products/product');
 const review = require('./controllers/reviews/review');
-module.exports = (app) => {
+module.exports = function(app) {
+
 
 
     app.use(bodyParser.text())
 
-    app.use('/api/user', user);
+    app.use('/api/auth', user);
+
     app.use('/api/auth', login);
-    app.use('/api/product', product);
+
+    const checkAuth = (req, res, next) => {
+        console.log(req.body)
+        if (!req.headers.authorization) {
+            return res.status(403).json({ error: 'No credentials sent!' });
+        }
+
+        // check valid
+        console.log(req.headers.authorization);
+
+        next();
+    };
+    app.use(checkAuth);
+
+    app.use('/api/products', product);
     app.use('/api/review', review);
 
     //invalid url
--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express();
 var http = require('http')
 
 require('./app.js')(app);
-http.createServer(app).listen(3000, () => {
+http.createServer(app).listen(4000, () => {
     console.log('Server Created')
 })
